91fe4a28ee6b175f1853876707a89dce5f9ca55e
Komitaji <komitaji@outlook.com>
2022-05-06 20:32:03 +0530

    implement the intial part of the landing page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/button.scss';
 
-export default function Button({ secondary, children }: { secondary?: boolean, children: string }) {
+export default function Button({ secondary, children, style }: { secondary?: boolean, children: string, style?: Record<string, string>}) {
   if (secondary) {
-    return <button className="secondary" type="button">{ children }</button>
+    return <button className='secondary' type='button' style={style}>{ children }</button>
   }
-  return <button className="primary" type="button">{ children }</button>
+  return <button className='primary' type='button' style={style}>{ children }</button>
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,21 +5,29 @@ import '../styles/landing_page.scss';
 
 export default function LandingPage() {
   return (
-    <div className="root">
-      <div className="first-part">
+    <div className='root'>
+      <div className='first-part'>
         <nav>
-          <div>
-            <img src={logo} alt="logo" />
-            <a href="/">Marketplace</a>
-            <a href="/">Dashboard</a>
-            <a href="/">Governance</a>
+          <div className='left-nav'>
+            <img src={logo} alt='logo' />
+            <a href='/'>Marketplace</a>
+            <a href='/'>Dashboard</a>
+            <a href='/'>Governance</a>
           </div>
-          <div>
+          <div className='right-nav'>
             <Button secondary={true}>Buy USDC</Button>
             <Button secondary={true}>Network</Button>
             <Button>Connect Wallet</Button>
           </div>
         </nav>
+        <div>
+          <h1>Marketplace for rentable billboards in the metaverse</h1>
+          <p>Zesty Market is a web3-enabled and tokenized advertising marketplace for Internet Communities owned by Internet Communities.</p>
+          <div>
+            <Button style={{ height: '60px', marginRight: '20px' }}>Launch App</Button>
+            <Button secondary={true} style={{ height: '60px' }}>Learn more</Button>
+          </div>
+        </div>
       </div>
     </div>
   );
